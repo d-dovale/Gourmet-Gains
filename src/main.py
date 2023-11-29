b7fa7566891810041ea54dc89824852b56dd3b37
danieldsouza13 <daniel.dsouza@ufl.edu>
2023-11-29 17:04:17 -0500

    added menu options

--- src/main.py
+++ src/main.py
@@ -7,6 +7,24 @@ print("Main Menu:\n")
 
 while True:
     print("Please enter a food item to search for, or type 'quit' to exit.")
-    print("1. Input Item\n2. Search Single Macronutrient\n3. Search All Macronutrients\n4. Search Single Nutrient\n4. Exit\n")
+    print("1. Input Item\n2. Search Single Macronutrient\n3. Search All Macronutrients\n4. Search Single Nutrient\n5. Exit\n")
     choice = input("Pick an Option: ")
+
+    if(choice == 1) :
+        pass
+
+    elif(choice == 2) :
+        pass
+
+    elif(choice == 3):
+        pass
+
+    elif(choice == 4):
+        pass
+
+    elif(choice == 5):
+        break
+
+
+
 
