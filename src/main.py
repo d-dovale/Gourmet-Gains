ba96a9830a8e02c3c3646438fb10316b96f52c87
d-dovale <ddovale2004@gmail.com>
2023-11-29 17:56:09 -0500

    added main menu function and search food function

--- src/main.py
+++ src/main.py
@@ -2,27 +2,36 @@ import food
 
 report = food.get_report()
 
-print("WELCOME TO GOURMET GAINS!\n")
-print("Main Menu:")
+def main_menu():
+    print("WELCOME TO GOURMET GAINS!\n")
+    print("Main Menu:")
 
-while True:
-    print("\n1. Input Item\n2. Search Single Macronutrient\n3. Search All Macronutrients\n4. Search Single Nutrient\n5. Exit\n")
-    choice = int(input("Pick an Option: "))
+    while True:
+        print("\n1. Input Item\n2. Search Single Macronutrient\n3. Search All Macronutrients\n4. Search Single Nutrient\n5. Exit\n")
+        choice = int(input("Pick an Option: "))
 
-    if(choice == 1):
-        pass
+        if(choice == 1):
+            pass
 
-    elif(choice == 2):
-        pass
+        elif(choice == 2):
+            pass
 
-    elif(choice == 3):
-        pass
+        elif(choice == 3):
+            pass
 
-    elif(choice == 4):
-        pass
+        elif(choice == 4):
+            pass
 
-    elif(choice == 5):
-        break
+        elif(choice == 5):
+            break
+
+def search_food(food_item):
+    food_report = food.get_report()
+    search_results = [item for item in food_report if food_item.lower() in item['Description'].lower()]
+    return search_results
+
+if __name__ == '__main__':
+    print("hello world")
 
 
 
