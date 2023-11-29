fb45fc5386729dda38699a924104fd7000b5f4c4
d-dovale <ddovale2004@gmail.com>
2023-11-29 17:06:01 -0500

    changed

--- src/main.py
+++ src/main.py
@@ -6,8 +6,6 @@ print("WELCOME TO GOURMET GAINS!\n")
 print("Main Menu:")
 
 while True:
-    print("Please enter a food item to search for, or type 'quit' to exit.")
-    print("1. Input Item\n2. Search Single Macronutrient\n3. Search All Macronutrients\n4. Search Single Nutrient\n4. Exit\n")
     print("\n1. Input Item\n2. Search Single Macronutrient\n3. Search All Macronutrients\n4. Search Single Nutrient\n4. Exit\n")
     choice = input("Pick an Option: ")
 
