fad3e6e80b1671effa28fcf52cf43d4a03adcab3
d-dovale <ddovale2004@gmail.com>
2023-11-29 16:57:00 -0500

    changed main

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ import food
 report = food.get_report()
 
 print("WELCOME TO GOURMET GAINS!\n")
-print("Main Menu:\n")
+print("Main Menu:")
 
 while True:
-    print("1. Input Item\n2. Search Single Macronutrient\n3. Search All Macronutrients\n4. Search Single Nutrient\n4. Exit\n")
+    print("\n1. Input Item\n2. Search Single Macronutrient\n3. Search All Macronutrients\n4. Search Single Nutrient\n4. Exit\n")
     choice = input("Pick an Option: ")
 
